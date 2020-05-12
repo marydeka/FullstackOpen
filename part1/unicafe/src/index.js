46c1e54c0b9555eeda6919d8bbfdf65c64a97c12
Mary Deka <mary.m.deka@gmail.com>
2020-05-11 22:46:45 -0500

    finished part 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -28,17 +28,24 @@ const App = () => {
 
 const Statistics = ({good, neutral, bad}) => {
 
-
+  if (good === 0 && neutral === 0 && bad === 0){
+    return (
+      <>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </>
+    )
+  }
   return (
     <>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>average {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}</p>
-    <p>positive {good / (good + neutral + bad) * 100}%</p>
-    </>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad) * 100}%</p>
+      </>
   )
 }
 
